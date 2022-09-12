4c939a6578f8616dfa08d34859f8b40a3fca171f
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-09-13 00:38:08 +0300

    postSlice

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 // import {sub} from "date-fns"
 
-const initialState = [
+const initialState=[]
     // {
     //     id: '1',
     //     title: 'Learning Redux Toolkit',
@@ -26,12 +26,12 @@ const initialState = [
     //         rocket: 0,
     //     }
     // }
-    ...JSON.parse(localStorage.getItem("posts"))
-]
+    // [...JSON.parse(localStorage.getItem("posts"))]
+
 
 const postsSlice=createSlice({
     name:"post",
-    initialState,
+    initialState:[...JSON.parse(localStorage.getItem("posts"))],
     reducers:{
         addPost(state,{payload}){
             state.push(payload)
